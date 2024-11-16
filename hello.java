4307b52d331b834534a325ae7b2697cea072583d
Gorish Gupta <gorishgupta50@gmail.com>
2024-11-16 23:50:35 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 public class hello{
     public static void main(String args[]){
+        for(int i=0;i<5;i++){
         System.out.println("Hello World");
+        }
         System.out.println("aRI gATO gOZIMUS");
     }
 }
