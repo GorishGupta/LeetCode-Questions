85567e90d0a70ce4e3a7cf44c583ff152f6433bb
Gorish Gupta <gorishgupta50@gmail.com>
2024-11-18 00:36:28 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,21 @@
 public class hello{
     public static void main(String args[]){
        int x = 5;
-       for(int i=0;i<=x;i++){
-        for(int j=0;j<i;j++){
-            System.out.print("*");
+       for(int i=0;i<x;i++){
+        for(int j=x-i;j>0;j--){
+            System.out.print("   ");
+        }
+        for(int j=0;j<2*i-1;j++){
+            System.out.print(" * ");
+        }
+        System.out.println();
+       }
+       for(int i=x;i>0;i--){
+        for(int j=x-i;j>0;j--){
+            System.out.print("   ");
+        }
+        for(int j=0;j<2*i-1;j++){
+            System.out.print(" * ");
         }
         System.out.println();
        }
