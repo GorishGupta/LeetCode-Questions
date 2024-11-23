b03aa34c1d972e773f5366ee8a7d2d5ef63c7f81
Gorish Gupta <gorishgupta50@gmail.com>
2024-11-23 22:50:44 +0530

    Create butterfly.java

---
+++ butterfly.java
@@ -0,0 +1,35 @@
+public class butterfly {
+    public static void main(String[] args) {
+        int x =5;
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for(int i=x-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
