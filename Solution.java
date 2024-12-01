c906dd6043dc9d4e235b96f7567e90a0381a18d2
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-01 19:46:10 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,10 +2,10 @@ public class Solution {
     public boolean checkifExist(int arr[]){
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
-                if(i!=j){
-                    if(arr[i] == 2*arr[j]){
+                if(i!=j && arr[i] == 2*arr[j] ){
+                   // if(){
                         return true;
-                    }
+                    //}
                  //   continue;
                 }
                 
