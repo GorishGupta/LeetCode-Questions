5b27cb54e7b8409e71bdb818aa989a788a147b9b
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-01 19:41:27 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean checkifExist(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i==j){
+                    continue;
+                }
+                if(arr[i]== 2*arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+}
