44acad6fc1f2d82e1a4bdf9c9aa9ccab1e475607
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-02 12:57:48 +0530

    Update DS1455.java

--- DS1455.java
+++ DS1455.java
@@ -9,3 +9,15 @@ class Solution {
        return -1;
        }
 }
+//2185
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count =0;
+        for(int i=0;i<words.length;i++){
+            if(words[i].startsWith(pref)){
+                count++;
+            }
+        }
+        return count;        
+    }
+}
