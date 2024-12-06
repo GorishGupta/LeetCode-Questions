dcb8a2bb69dad3c8b074451ab4a9e062e5d29e29
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-07 00:28:10 +0530

    Update butterfly.java

--- butterfly.java
+++ butterfly.java
@@ -31,5 +31,35 @@ public class butterfly {
             }
             System.out.println();
         }
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for(int i=x-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=x-i;j>=1;j--){
+                System.out.print("   ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
     }
 }
