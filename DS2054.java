3c1cced714b56bba20bac6b7d00d2f5969d24de3
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-09 00:07:02 +0530

    Update DS2054.java

--- DS2054.java
+++ DS2054.java
@@ -26,15 +26,6 @@ class Solution {
         int max = 0;
         int ans = 0;
 
-        for(int i = 0; i < events.length; i++) {
-            while(!pq.isEmpty() && pq.peek()[1] < events[i][0]) {
-                max = Math.max(max, pq.poll()[2]);
-            }
-
-            ans = Math.max(ans, max + events[i][2]);
-            pq.offer(events[i]);
-        }
-
         return ans;
     } 
 }
