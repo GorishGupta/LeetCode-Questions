50d696487985de64761fd3500ea5fe38c7f9666d
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-11 15:25:39 +0530

    Update DS2779.java

--- DS2779.java
+++ DS2779.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
-        /*
+        //bubble sort
         for(int i=1;i<=nums.length;i++){
             for(int j=0;j<nums.length - i;j++){
                 while(nums[j] > nums[j+1]){
@@ -9,8 +9,9 @@ class Solution {
                     nums[j+1] = temp;
                 }
             }
-        }*/
-        Arrays.sort(nums);
+        }
+        //inbuild sorting
+       // Arrays.sort(nums);
         int i=0,j,n= nums.length;
         for(j=0;j<n;j++){
             if(nums[j] - nums[i] > k*2){
