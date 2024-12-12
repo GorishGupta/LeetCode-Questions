a246bc4978752c1b2d1d9c1833b912919c97bb7c
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-12 15:32:09 +0530

    Create DS2558.java

---
+++ DS2558.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        long ans = 0;
+        int n = gifts.length;
+        Arrays.sort(gifts);
+
+        while(k > 0){
+            gifts[n-1] = (int) Math.sqrt(gifts[n-1]);
+            Arrays.sort(gifts);
+            k--;
+        }
+        for(int i=0;i<n;i++){
+            ans += gifts[i];
+        }
+        return ans;
+
+    }
+}
