366a568162f29fce03b5e9287b8bf320dfa91869
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-17 11:59:03 +0530

    Update DS2182.java

--- DS2182.java
+++ DS2182.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[s.charAt(i) - 'a']++;
         }
-
+        System.out.println();
         for(int i=25;i>=0;i--){
             if(arr[i]==0){
                 continue;
@@ -31,7 +31,7 @@ class Solution {
                 }
             } 
         }
-        
+        System.out.println();
         return sb.toString();
     }
 }
