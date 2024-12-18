2a8db4d139140af15357e2c3a7133046dbb25fb2
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-18 22:33:52 +0530

    Update DS1475.java

--- DS1475.java
+++ DS1475.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
+
         int result[] = prices.clone();
+        
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(prices[j] <= prices[i]){
