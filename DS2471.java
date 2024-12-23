c14af7c7d5b94376f7eec85765c43bde0522c03c
Gorish Gupta <gorishgupta50@gmail.com>
2024-12-23 19:50:36 +0530

    Update DS2471.java

--- DS2471.java
+++ DS2471.java
@@ -1,5 +1,18 @@
 import java.util.*;
+
 class DS2471 {
+    public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode() {}
+             TreeNode(int val) { this.val = val; }
+             TreeNode(int val, TreeNode left, TreeNode right) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+             }
+         }
     public int minimumOperations(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
